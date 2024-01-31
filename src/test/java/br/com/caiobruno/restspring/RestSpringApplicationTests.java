7b0e9499084bd343c36ede5c1d51aab9a8683584
Caio Bruno <caiobruno90@yahoo.com>
2024-01-31 14:14:44 -0300

    inicio projeto restfull

---
+++ src/test/java/br/com/caiobruno/restspring/RestSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.caiobruno.restspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
