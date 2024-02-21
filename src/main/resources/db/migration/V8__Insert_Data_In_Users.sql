e8bb37bb8b9b507e730f061263f48d16cce331eb
Caio Bruno <caiobruno90@yahoo.com>
2024-02-21 11:10:05 -0300

    Atualizado projeto

--- src/main/resources/db/migration/V8__Insert_Data_In_Users.sql
+++ src/main/resources/db/migration/V8__Insert_Data_In_Users.sql
@@ -1,4 +1,5 @@
 ﻿INSERT INTO `users` (`user_name`, `full_name`, `password`, `account_non_expired`, `account_non_locked`, `credentials_non_expired`, `enabled`) VALUES
 	('leandro', 'Leandro Costa', '2272b300f62e0e87ced12bee745b8afe99daa1a5f93753254eda000598891d5cb7afd6ca64d9c73c', b'1', b'1', b'1', b'1'),
 	('flavio', 'Flavio Costa', '2272b300f62e0e87ced12bee745b8afe99daa1a5f93753254eda000598891d5cb7afd6ca64d9c73c', b'1', b'1', b'1', b'1'),
-	('caio', 'Caio Bruno', '9a74fdeefffaea765cf2e933c608c6c7e842381141690f842fc03639dbb0f823b08f271d07a6e132', b'1', b'1', b'1', b'1');
+	('caio','Caio Bruno','9a74fdeefffaea765cf2e933c608c6c7e842381141690f842fc03639dbb0f823b08f271d07a6e132', b'1', b'1', b'1', b'1');
+
--- src/test/java/br/com/caiobruno/restspring/configs/TestConfigs.java
+++ src/test/java/br/com/caiobruno/restspring/configs/TestConfigs.java
@@ -4,7 +4,7 @@ public class TestConfigs {
 
     public static final int SERVER_PORT = 8888;
 
-    public static final int API_PORT = 8081;
+    public static final int API_PORT = 8080;
 
     public static final String HEADER_PARAM_AUTHORIZATION = "Authorization";
     public static final String HEADER_PARAM_ORIGIN = "Origin";
